e53dcbf879b61ac8c181e9d69bc95b772163cc92
Ami-tnk <ami1y251992@gmail.com>
2021-01-23 08:22:37 +0000

    [Update]コメント機能の非同期化

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,17 @@
 class PostCommentsController < ApplicationController
   def create
-    cook = Cook.find(params[:cook_id])
-    comment = PostComment.new(post_comment_params)
-    comment.user_id = current_user.id
-    comment.cook_id = cook.id
-    comment.save
-    redirect_to cook_path(cook), notice: "コメントしました！"
+    @cook = Cook.find(params[:cook_id])
+    @post_comment = PostComment.new(post_comment_params)
+    @post_comment.user_id = current_user.id
+    @post_comment.cook_id = @cook.id
+    @post_comment.save
+    # app/views/book_comments/create.js.erbを参照する
   end
 
   def destroy
+    @cook = Cook.find(params[:cook_id])
     PostComment.find_by(id: params[:id], cook_id: params[:cook_id]).destroy
-    redirect_to cook_path(params[:cook_id]), notice: "コメントを削除しました。"
+    # app/views/book_comments/destroy.js.erbを参照する
   end
 
   private
