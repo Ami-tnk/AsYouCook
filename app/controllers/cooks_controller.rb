ba7505b2bb03f6896330af321b4175f49a77dafe
Ami-tnk <ami1y251992@gmail.com>
2021-01-31 04:01:52 +0000

    [Add]user登録してない人のlink, layout_update

--- app/controllers/cooks_controller.rb
+++ app/controllers/cooks_controller.rb
@@ -1,5 +1,5 @@
 class CooksController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @cooks
@@ -10,14 +10,21 @@ class CooksController < ApplicationController
   end
 
   def show
-    @cook = Cook.find(params[:id])
-    @user = User.find(@cook.user_id)
-    @post_comment = PostComment.new
-    # もし未確認の通知があれば、通知先の投稿詳細画面まで行くと確認済みになるようにする
-    if  @unchecked_notifications = current_user.passive_notifications.where(cook_id: @cook.id, checked: false)
-      @unchecked_notifications.each do |notification|
-        notification.update_attributes(checked: true)
+    if user_signed_in?
+      @cook = Cook.find(params[:id])
+      @user = User.find(@cook.user_id)
+      @post_comment = PostComment.new
+      # もし未確認の通知があれば、通知先の投稿詳細画面まで行くと確認済みになるようにする
+      if  @unchecked_notifications = current_user.passive_notifications.where(cook_id: @cook.id, checked: false)
+        @unchecked_notifications.each do |notification|
+          notification.update_attributes(checked: true)
+        end
       end
+    else
+      # ユーザ登録,ログインしていない人用
+      @cook = Cook.find(params[:id])
+      @user = User.find(@cook.user_id)
+      @post_comment = PostComment.new
     end
   end
 
