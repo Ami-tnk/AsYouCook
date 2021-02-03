3e4c089a94d83c24c359a8ee07a3a0aeb1cd01e3
Ami-tnk <ami1y251992@gmail.com>
2021-02-03 12:06:25 +0000

    [Update]RSpec/system_spec/cooks#update,destroy

--- spec/system/cooks_spec.rb
+++ spec/system/cooks_spec.rb
@@ -47,6 +47,35 @@ describe '料理投稿機能', type: :system do
     end
   end
 
+    describe '新規投稿した時のテスト' do
+    context '投稿した時' do
+      before do
+        visit new_user_session_path
+        fill_in 'user[email]', with: user.email
+        fill_in 'user[password]', with: user.password
+        click_button 'ログインする'
+        visit mypage_path
+      end
+
+      it '自分の新しい投稿が正しく保存される' do
+        attach_file 'cook[image]', "spec/fixtures/test.jpg"
+        fill_in 'cook[cooking_name]', with: cook.cooking_name
+        fill_in 'cook[recipe]', with: cook.recipe
+        click_button '保存'
+        expect(page).to have_content "料理を投稿しました!"
+      end
+      it '投稿に失敗する' do
+	        click_button '保存'
+	        expect(page).to have_content 'エラー'
+	        expect(current_path).to eq('/mypage')
+	    end
+      it 'リダイレクト先がmypageになっている' do
+        click_button '保存'
+        expect(current_path).to eq '/mypage'
+      end
+    end
+  end
+
   describe '料理詳細画面のテスト' do
     context 'userがログインしていない時' do
       before do
@@ -86,13 +115,14 @@ describe '料理投稿機能', type: :system do
       it '投稿の編集リンクが表示される' do
         expect(page).to have_link '編集', href: edit_cook_path(cook)
       end
-      it '投稿の削除リンクが表示される' do
+      it '投稿の削除リンクが表示され、問題なく削除される' do
         expect(page).to have_link '削除', href: cook_path(cook)
+        expect{ cook.destroy }.to change{ Cook.count }.by(-1)
       end
     end
   end
 
-  describe '料理編集画面のテスト'do
+  describe '料理編集画面のテスト' do
     context '画面を表示した時' do
       before do
         visit new_user_session_path
@@ -117,34 +147,35 @@ describe '料理投稿機能', type: :system do
         expect(page).to have_link '戻る', href: cook_path(cook)
       end
     end
-  end
 
-  describe '新規投稿した時のテスト' do
-    context '投稿した時' do
+    context '更新処理に関するテスト' do
       before do
         visit new_user_session_path
         fill_in 'user[email]', with: user.email
         fill_in 'user[password]', with: user.password
         click_button 'ログインする'
-        visit mypage_path
+        visit cook_path(cook)
+        cook.user_id == user.id
+        visit edit_cook_path(cook)
       end
-
-      it '自分の新しい投稿が正しく保存される' do
-        attach_file 'cook[image]', "spec/fixtures/test.jpg"
-        fill_in 'cook[cooking_name]', with: cook.cooking_name
-        fill_in 'cook[recipe]', with: cook.recipe
+      it '更新に成功しサクセスメッセージが表示されるか' do
+        fill_in 'cook[cooking_name]', with: Faker::Lorem.characters(number:8)
+        fill_in 'cook[recipe]', with: Faker::Lorem.characters(number:30)
         click_button '保存'
-        expect(page).to have_content "料理を投稿しました!"
+        expect(page).to have_content '料理を編集しました!'
       end
-      it '投稿に失敗する' do
-	        click_button '保存'
-	        expect(page).to have_content 'エラー'
-	        expect(current_path).to eq('/mypage')
+      it '更新に失敗しエラーメッセージが表示されるか' do
+        fill_in 'cook[cooking_name]', with: ""
+        fill_in 'cook[recipe]', with: ""
+        click_button '保存'
+        expect(page).to have_content 'エラー'
+      end
+      it '更新後のリダイレクト先は正しいか' do
+        fill_in 'cook[cooking_name]', with: Faker::Lorem.characters(number:8)
+        fill_in 'cook[recipe]', with: Faker::Lorem.characters(number:30)
+        click_button '保存'
+        expect(page).to have_current_path cook_path(cook)
 	    end
-      it 'リダイレクト先がmypageになっている' do
-        click_button '保存'
-        expect(current_path).to eq '/mypage'
-      end
     end
   end
 end
