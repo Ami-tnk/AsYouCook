2d28a4e9f4cd9b92a72a2aa45bbf34ee0ea8cc75
Ami-tnk <ami1y251992@gmail.com>
2021-02-08 07:33:21 +0000

    [Fixed]factorybot:nickname

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname { Faker::Name.name }
+    nickname { Faker::Lorem.characters(number: 10) }
     email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
